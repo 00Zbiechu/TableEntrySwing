ba8507360cafd2c14226ac0d0622595ececedb9f
Mateusz Zbiewski <00Zbiewski@gmail.com>
2022-04-02 18:03:31 +0200

    Poczatek towrzenia aplikacji

---
+++ src/app/Window.java
@@ -0,0 +1,19 @@
+package app;
+
+import javax.swing.*;
+
+public class Window extends JFrame {
+
+    //stałe do tworzenia rozmiaru okna
+    private final int WIDTH = 600;
+    private final int HEIGHT = 400;
+
+
+    //konstruktor klasy Window
+    Window(){
+
+
+
+    }
+
+}
